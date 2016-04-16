c401a0a47adaa01e0f390647bc3d907a481ca03f
Gomozov <valeriy.gomozov@gmail.com>
2016-04-16 17:52:22 +0300

    Add bcrypt-ruby

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,6 +4,21 @@ describe "StaticPages" do
   let(:base_title) { "Ruby on Rails Tutorial Sample App" }
   subject {page}
 
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    expect(page).to have_title(full_title('About Us'))
+    click_link "Help"
+    expect(page).to have_title(full_title('Help'))
+    click_link "Contact"
+    expect(page).to have_title(full_title('Contact'))
+    click_link "Home"
+    click_link "Sign up now!"
+    expect(page).to have_title(full_title('Sign up'))
+    click_link "sample app"
+    expect(page).to have_title(full_title(''))
+  end
+
   describe "Home page" do
     before {visit root_path}
     it { should have_content('Sample App') }
@@ -26,6 +41,7 @@ describe "StaticPages" do
   describe "Contact page" do
     before {visit contact_path}
     it { should have_content('Contact') }
+    it { should have_selector('h1', text: 'Contact') }
     it { should have_title(full_title('Contact')) }
   end
 end
